3c177729bec1c58805b222b6da0154f5c125df03
ctcpip <ctcpip@users.noreply.github.com>
2026-01-21 20:13:20 -0600

    ⬆️ bump deps

--- eslint.config.js
+++ eslint.config.js
@@ -2,4 +2,16 @@ const ultraMegaConfig = require('eslint-config-ultra-mega');
 
 module.exports = [
   ...ultraMegaConfig,
+  {
+    languageOptions: {
+      globals: {
+        clearInterval: 'false',
+        console: 'false',
+        module: 'false',
+        process: 'false',
+        require: 'false',
+        setInterval: 'false',
+      },
+    },
+  },
 ];
